c5787bd59911730fdac61c415da5510ffb63be20
Nisha <117558347+nishapoojary7@users.noreply.github.com>
2023-02-01 22:10:52 +0530

    Add files via upload

---
+++ even.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+  int n;
+  printf("enter the numbers");
+  scanf("%d",&n);
+  if(n%2==0)
+  {
+    printf("even");
+  }
+  else
+  {
+printf("not even");
+  }
+
+}else
+    {
+    printf("%c is not a vowel");
